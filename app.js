d177cf75baebc9c8c9563081cbb97fc52df3f364
Shaggy12vD <92492779+Shaggy12vD@users.noreply.github.com>
2021-10-13 17:47:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navlinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
+
+
+
+
